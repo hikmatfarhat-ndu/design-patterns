e9c41b0445d29736b41c3f3c59b4b3f423670452
Hikmat Farhat <h.farhat@soton.ac.uk>
2023-10-26 08:24:50 +0100

    comment why abstract class is needed

--- decorator/RedShapeDecorator.java
+++ decorator/RedShapeDecorator.java
@@ -1,4 +1,9 @@
 package decorator;
+/* we could have implemented the Shape interface directly
+ * instead of using abstract class ShapeDecorator
+ * But we would have to implement many "common" methods
+ * in each implementation class
+ */
 public class RedShapeDecorator extends ShapeDecorator {
     public RedShapeDecorator(Shape decoratedShape){
         super(decoratedShape);
